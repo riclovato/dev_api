d7be050566390441bba433ee944deee347bfe43c
riclovato <riic.lovato@gmail.com>
2024-02-24 17:16:32 -0300

    GET all, POST

--- app.py
+++ app.py
@@ -3,29 +3,47 @@ import json
 
 app = Flask(__name__)
 
-developers = [{'name':'Ricardo',
-               'skills':['Python','Flask']},
-              {'name':'Galleani',
-               'skills':['Python','Django']}]
-@app.route('/dev/<int:id>/', methods=['GET','PUT','DELETE'])
+developers = [{'id': '0',
+               'name': 'Ricardo',
+               'skills': ['Python', 'Flask']},
+              {'id': '1',
+               'name': 'Galleani',
+               'skills': ['Python', 'Django']}]
+
+
+# returns, add and delete a developer by id
+@app.route('/dev/<int:id>/', methods=['GET', 'PUT', 'DELETE'])
 def developer(id):
     if request.method == 'GET':
         try:
             response = developers[id]
         except IndexError:
             message = 'Developer id {} does not exist.'.format(id)
-            response = {'status':'error', 'message': message}
+            response = {'status': 'error', 'message': message}
         except Exception:
             message = 'Unknown error. Contact API admin.'
             response = {'status': 'error', 'message': message}
         return jsonify(response)
     elif request.method == 'PUT':
-         info = json.loads(request.data)
-         developers[id] = info
-         return jsonify(info)
+        info = json.loads(request.data)
+        developers[id] = info
+        return jsonify(info)
     elif request.method == 'DELETE':
         developers.pop(id)
-        return jsonify({'status':'successful','message':'Record deleted'})
+        return jsonify({'status': 'successful', 'message': 'Record deleted'})
+
+
+# show all developers and add a new developer
+@app.route('/dev/', methods=['POST', 'GET'])
+def developer_list():
+    if request.method == 'POST':
+        info = json.loads(request.data)
+        position = len(developers)
+        info['id'] = position
+        developers.append(info)
+        return jsonify({'status': 'success', 'message': 'Entry recorded.'})
+    elif request.method == 'GET':
+        return jsonify(developers)
 
 
 if __name__ == '__main__':
